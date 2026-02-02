15fe72b50692f00a33933ad023264e02b405a757
João Pedro Marretto <joaopedromh@hotmail.com>
2026-02-02 08:58:49 -0300

    Adiciona Tratamento de Datas

--- delivery_analysis.py
+++ delivery_analysis.py
@@ -24,6 +24,7 @@ print("-"*40)
 missing_values = df.isnull().sum()
 missing_percentage = (missing_values / len(df)) * 100 
 
+
 # cria dataframe para valores nulos
 missing_df = pd.DataFrame({
     'Valores ausentes (soma)' : missing_values,
@@ -32,6 +33,7 @@ missing_df = pd.DataFrame({
 
 
 print(missing_df[missing_df['Valores ausentes (soma)'] > 0])
+
 if missing_df['Valores ausentes (soma)'].sum() == 0:
     print("Nenhum valor ausente encontrado!")
 print()
@@ -40,7 +42,7 @@ print()
 # verificar tempos de entrega muito baixos
 
 delivery_outlier = df[df['Delivery_Time'] < 15].shape[0]
-print(f'Pedidos com tempo de entrega menor que 10 minutos: {delivery_outlier} ({delivery_outlier/len(df)*100:.2f}%)')
+print(f'Pedidos com tempo de entrega menor que 15 minutos: {delivery_outlier} ({delivery_outlier/len(df)*100:.2f}%)')
 
 # limpeza de dados
 
@@ -48,8 +50,52 @@ print(f'Pedidos com tempo de entrega menor que 10 minutos: {delivery_outlier} ({
 
 df_clean = df.copy()
 
-# Agent_Rating nulos
+# remover nulos
 
-missing_rating = df_clean[df_clean('Agent_Rating').isnull()]
+df_clean = df_clean.dropna()
+
+rows_cleaned = df.shape[0] - df_clean.shape[0]
+percentage_cleaned = (rows_cleaned / df.shape[0]) * 100
+print(f"deletadas {rows_cleaned} linhas, totalizando um percentual de {percentage_cleaned:.2f}%")
+
+
+# tratamento das datas
+
+df_clean['Order_Date'] = pd.to_datetime(df_clean['Order_Date'], format = '%Y-%m-%d')
+df_clean['Order_Time'] = pd.to_datetime(df_clean['Order_Time'], format = '%H:%M:%S')
+df_clean['Pickup_Time'] = pd.to_datetime(df_clean['Pickup_Time'], format = '%H:%M:%S')
+
+df_clean['Order_DateTime'] = pd.to_datetime(
+    df_clean['Order_Date'].astype(str) + ' ' + df_clean['Order_Time'].astype(str)
+    )
+
+# dt.dayofweek : Monday=0, Sunday=6.
+df_clean['Order_Weekday'] = df_clean['Order_DateTime'].dt.dayofweek
+
+# hora do dia
+df_clean['Order_Hour'] = df_clean['Order_DateTime'].dt.hour
+
+# determinar periodo do dia
+
+def get_time_period(hour):
+    if 5 <= hour < 12:
+        return 'Manha'
+    elif 12 <= hour < 18:
+        return 'Tarde'
+    elif 18 <= hour < 23:
+        return 'Noite'
+    else:
+        'Madrugada'
+
+df_clean['Time_Period'] = df_clean['Order_Hour'].apply(get_time_period)
+
+# verificar resultados
+
+print("resultados após tratamento")
+print(df_clean[['Order_DateTime', 'Order_Weekday', 'Order_Hour', 'Time_Period']].head())
+
+
+
+
 
 
